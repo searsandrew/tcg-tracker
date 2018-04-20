0bac6b489b487735d5c12f7477c90fa056a09892
Andrew Sears <andrew@mayfifteenth.com>
2018-04-19 23:32:54 -0500

    Move token auth to base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
+
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    public function token()
+    {
+        $client = new Client();
+        $res = $client->request('POST', 'https://api.tcgplayer.com/token', [
+            'form_params' => [
+                'grant_type' => 'client_credentials',
+                'client_id' => env('TCG_PUBLIC_KEY'),
+                'client_secret' => env('TCG_PRIVATE_KEY')
+            ]
+        ]);
+
+        return $res->getBody();
+    }
 }
